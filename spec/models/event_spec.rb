a0f60f647977c6d2a0e7e3c2fbdf07346b28bbe4
Sasha Grodzins <sasha.grodzins@devmynd.com>
2014-10-28 12:01:01 -0500

    formatting spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 describe Event do
-	context "assocations" do
-		it { should have_many :inventory}
-		it { should have_many(:involved_vendors).through(:inventory)}
-		it { should belong_to :team }
-	end
+  context "assocations" do
+    it { should have_many :inventory}
+    it { should have_many(:involved_vendors).through(:inventory)}
+    it { should belong_to :team }
+  end
 end
