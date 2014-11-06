2733faa93d58b865cb1e0d8649d5cba0b7eec994
Sasha Grodzins <sasha.grodzins@devmynd.com>
2014-11-06 09:58:19 -0600

    Create new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,26 @@ class EventsController < ApplicationController
   def index
     @events = Event.all.order("date DESC")
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.create(event_params)
+
+    if @event.save
+      redirect_to events_path, notice: "Event successfully created"
+    else
+      flash.now[:alert] = "Could not save event...."
+      @errors = @event.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :team_id, :location)
+  end
 end
