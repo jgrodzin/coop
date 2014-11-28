b1aae137bdc36d03423ce22d99de904825225a68
Sasha Grodzins <sasha.grodzins@devmynd.com>
2014-11-28 15:17:33 -0600

    remove pry from spec. might be messing with herkou

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -6,12 +6,11 @@ describe CartItem do
     it { should belong_to :product }
   end
 
-  it "is put into a user's shopping cart" do
+  it "#add_item" do
     member = FactoryGirl.create(:member)
     shopping_cart = FactoryGirl.create(:shopping_cart, member: member)
     product = FactoryGirl.create(:product, name: "Choco Taco")
-    # cart_item = FactoryGirl.create(:cart_item)
-    binding.pry
+
     shopping_cart.add_item(product)
     expect(shopping_cart.cart_items.map(&:product)).to include(product)
   end
