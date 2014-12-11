68c29cb119a067fd174b7f026e1db73ad78ee010
Sasha Grodzins <sasha.grodzins@devmynd.com>
2014-12-10 19:37:45 -0600

    separating shopping at event from adding to event.

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -3,8 +3,8 @@ class ShoppingCartsController < ApplicationController
 
   def index
     @event = Event.find(params[:event_id])
-    @products = @event.products
-    # @shopping_cart = ShoppingCart.where(event_id: @event.id, member: current_member)
+    @products = @event.products.group_by(&:vendor)
+    @shopping_cart = ShoppingCart.find_or_create_by(event: @event, member: current_member)
   end
 
 ### not implemented
