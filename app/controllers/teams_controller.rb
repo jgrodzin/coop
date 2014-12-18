e1e29a4819d2ff01719c86575559c1c48f5ecd7e
Sasha Grodzins <sasha.grodzins@devmynd.com>
2014-12-17 20:23:40 -0600

    TODO: team CRUD

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,12 +1,36 @@
 class TeamsController < ApplicationController
   def index
-    unless current_member.teams.empty?
-      @teams = current_member.teams
-    end
+    @teams = current_member.teams unless current_member.teams.empty?
   end
 
   def show
     current_member
     @my_team = Team.find(params[:id])
+  end
+
+  def new
+    @team = Team.new(team_params)
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    @team.update(team_params)
+
+    if @team.save
+      redirect_to teams_admins_path, notice: "Team Successfully updated"
+    else
+      @errors = @team.errors.full_messages
+      render :edit, notice: "Could not update team!"
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :number)
   end
 end
