35aef6375108898ca2edd44af3a68d5de147640a
Sasha Grodzins <sasha.grodzins@devmynd.com>
2015-07-21 17:18:36 -0500

    seed with ACTIVE members

--- spec/factories.rb
+++ spec/factories.rb
@@ -33,7 +33,7 @@ FactoryGirl.define do
     zip { Faker::Address.zip_code }
     phone { Faker::PhoneNumber.phone_number }
     admin false
-    status Member.statuses[:archived]
+    status Member.statuses[:active]
   end
 
   factory :admin, parent: :member do
